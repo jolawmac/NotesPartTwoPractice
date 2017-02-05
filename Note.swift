a96d6b9889991d23fe104d819141c59d26cadb8a
Joshua McDonald <joshua_mcdonald@me.com>
2017-02-05 16:50:58 -0700

    Add Files

---
+++ Note.swift
@@ -0,0 +1,9 @@
+//
+//  Note.swift
+//  NotesPartTwoPractice
+//
+//  Created by Josh & Erica on 2/5/17.
+//  Copyright © 2017 Josh McDonald. All rights reserved.
+//
+
+import Foundation
---
+++ NoteController.swift
@@ -0,0 +1,9 @@
+//
+//  NoteController.swift
+//  NotesPartTwoPractice
+//
+//  Created by Josh & Erica on 2/5/17.
+//  Copyright © 2017 Josh McDonald. All rights reserved.
+//
+
+import Foundation
